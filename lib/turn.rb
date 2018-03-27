7997fcc9b88ec9230e14f059525366d0302d4dea
Matt Vierling <matt.vierling@wework.com>
2018-03-27 12:15:52 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -4,7 +4,7 @@ def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
   index = input_to_index(input)
-  if valid_move?(board, index) = true
+  if valid_move?(board, index) == true
     puts "success!"
   else
     puts "fail!"
